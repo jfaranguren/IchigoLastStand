4deb6fd20504d5eeb28e8518eb5c460410a72cf3
jfaranguren <jfaranguren@icesi.edu.co>
2023-08-31 17:45:24 -0500

    Carga inicial

---
+++ IchigoLastStand/doc/API/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ui","c":"IchigoLastStand","l":"IchigoLastStand()","u":"%3Cinit%3E()"},{"p":"ui","c":"IchigoLastStand","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
